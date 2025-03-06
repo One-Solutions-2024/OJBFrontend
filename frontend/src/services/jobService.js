3651d1869d883d316de724da9f8d609cf8d5bbe1
One-Solutions-2024 <onesolutions06092024@gmail.com>
2025-03-06 18:40:17 +0530

    first commit

--- frontend/src/services/jobService.js
+++ frontend/src/services/jobService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'http://localhost:5000/api';
+const API_BASE_URL = 'https://xrjobsboard.onrender.com';
 
 export const getJobs = async () => {
   const response = await axios.get(`${API_BASE_URL}/jobs`);
